c3fdfe854192dbfee547d40f63f7574c4525dfac
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2015-06-22 21:43:43 +0200

    ZEN: Add Ubuntu ureadahead support
    Squashed commit of the following:
    
    commit 77e875ad963d09ecd63580d0c021081533cf483a
    Author: Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
    Date:   Mon Jun 9 17:21:50 2014 +0200
    
        fix open_exec trace
    
    commit f2a40b03948dd612397a86f490a064feb2033cbe
    Author: Scott James Remnant <scott@ubuntu.com>
    Date:   Tue Oct 27 10:05:32 2009 +0000
    
        UBUNTU: SAUCE: (no-up) trace: add trace events for open(), exec() and uselib() (for v3.7+)
    
        BugLink: http://bugs.launchpad.net/bugs/462111
    
        This patch uses TRACE_EVENT to add tracepoints for the open(),
        exec() and uselib() syscalls so that ureadahead can cheaply trace
        the boot sequence to determine what to read to speed up the next.
    
        It's not upstream because it will need to be rebased onto the syscall
        trace events whenever that gets merged, and is a stop-gap.
    
        [apw@canonical.com: updated for v3.7 and later.]
        BugLink: http://bugs.launchpad.net/bugs/1085766
        Signed-off-by: Scott James Remnant <scott@ubuntu.com>
        Acked-by: Stefan Bader <stefan.bader@canonical.com>
        Acked-by: Andy Whitcroft <andy.whitcroft@canonical.com>
        Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    
        Conflicts:
    
        	fs/open.c
    
        Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- fs/exec.c
+++ fs/exec.c
@@ -65,6 +65,8 @@
 #include <linux/vmalloc.h>
 #include <linux/random.h>
 
+#include <trace/events/fs.h>
+
 #include <linux/uaccess.h>
 #include <asm/mmu_context.h>
 #include <asm/tlb.h>
@@ -872,8 +874,10 @@ static struct file *do_open_execat(int fd, struct filename *name, int flags)
 	if (err)
 		goto exit;
 
-	if (name->name[0] != '\0')
+	if (name->name[0] != '\0') {
 		fsnotify_open(file);
+		trace_open_exec(name->name);
+	}
 
 out:
 	return file;
