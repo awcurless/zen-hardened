0fa8249cd415ba9914446e4087ad94b85483650d
Daniel Micay <danielmicay@gmail.com>
2017-05-03 21:54:56 -0400

    mm: add support for verifying page sanitization
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>
    Signed-off-by: Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>
    Signed-off-by: Levente Polyak <levente@leventepolyak.net>

--- kernel/power/snapshot.c
+++ kernel/power/snapshot.c
@@ -1150,6 +1150,9 @@ void clear_free_pages(void)
 	struct memory_bitmap *bm = free_pages_map;
 	unsigned long pfn;
 
+	if (!IS_ENABLED(CONFIG_PAGE_POISONING_ZERO) && !want_init_on_free())
+		return;
+
 	if (WARN_ON(!(free_pages_map)))
 		return;
 
@@ -1165,6 +1168,8 @@ void clear_free_pages(void)
 		memory_bm_position_reset(bm);
 		pr_info("free pages cleared after restore\n");
 	}
+	memory_bm_position_reset(bm);
+	pr_info("free pages cleared after restore\n");
 }
 
 /**
