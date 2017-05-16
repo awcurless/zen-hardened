e56a62d92be47df7cbb27b77e34eee613c24dadb
Daniel Micay <danielmicay@gmail.com>
2017-05-16 18:26:10 -0400

    restrict device timing side channels
    Based on the public grsecurity patches.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -958,13 +958,13 @@ static struct ctl_table kern_table[] = {
 	},
 #endif
 	{
-		.procname = "device_sidechannel_restrict",
-		.data = &device_sidechannel_restrict,
-		.maxlen = sizeof(int),
-		.mode = 0644,
-		.proc_handler = proc_dointvec_minmax_sysadmin,
-		.extra1 = &zero,
-		.extra2 = &one,
+		.procname	= "device_sidechannel_restrict",
+		.data		= &device_sidechannel_restrict,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax_sysadmin,
+		.extra1		= &zero,
+		.extra2		= &one,
 	},
 	{
 		.procname = "ngroups_max",
