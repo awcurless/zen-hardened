zen-kernel/zen-kernel

ZEN Linux kernel v4.19.2-zen1

Makefile
@@ -2,7 +2,7 @@
 VERSION = 4
 PATCHLEVEL = 19
 SUBLEVEL = 2
-EXTRAVERSION = -zen
+EXTRAVERSION = -zen1
 NAME = Neo Mir
 
 # *DOCUMENTATION*

awcurless/zen-hardened

Makefile
@@ -2,7 +2,7 @@
 VERSION = 4
 PATCHLEVEL = 19
 SUBLEVEL = 6
-EXTRAVERSION = -zen
+EXTRAVERSION = -a
 NAME = Neo Mir
 
 # *DOCUMENTATION*